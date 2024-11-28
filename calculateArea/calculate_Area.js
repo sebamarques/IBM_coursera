cdf92b38113da4081dd8e5c0d6db92dcc712b2f2
sebamarques <sebamarquezblanco@gmail.com>
2024-11-28 14:07:24 -0500

    functions excercise

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,11 @@ function calculateArea(){
     let area = length * width;
     document.getElementById("result").innerText = `The area of the rectangule is: ${area}`;
 }
+function groceryTracker(){
+    grocery1 = parseFloat(document.getElementById("Grocery1").value);
+    grocery2 = parseFloat(document.getElementById("Grocery2").value);
+    grocery3 = parseFloat(document.getElementById("Grocery3").value);
+
+    let totalAmount = grocery1 + grocery2 + grocery3;
+    document.getElementById("resultGrocery").innerText = `The total amount is:$ ${totalAmount}`
+}
