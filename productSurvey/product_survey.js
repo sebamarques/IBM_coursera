30358ea16ed701d9759492fb765c1540dbd92c91
sebamarques <sebamarquezblanco@gmail.com>
2024-11-28 17:30:52 -0500

    form excercise

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,7 +6,7 @@ function submitFeedback(){
     const designation = document.getElementById("designation").value;
     const productType = document.getElementById("productType").value;
     const feedback = document.getElementById("feedbackText").value;    
-    
+    const experience = document.getElementById("Experience").value;
     alert('Thank you for your valuable feedback');
     
     document.getElementById("userName").innerHTML = username;
@@ -16,7 +16,7 @@ function submitFeedback(){
     document.getElementById("userDesignation").innerHTML = designation;
     document.getElementById("userProductChoice").innerHTML = productType;
     document.getElementById("userFeedback").innerHTML = feedback;
-    
+    document.getElementById("userExperience").innerHTML = experience;    
     document.getElementById('userInfo').style.display = 'block';
 }
 const submitButton = document.getElementById("submitBtn");
